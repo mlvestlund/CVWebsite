8eb86f1bafdea8a102df02cdef03f5d6ff620c8d
mlvestlund <mariavestlund@yahoo.se>
2024-02-27 13:18:16 +0100

    Changed placement of images och did some formatting

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /* Easter egg 1: Darkmode-switch */
 const isDarkModePreferred = localStorage.getItem('darkMode');
 if (isDarkModePreferred === 'true') {
-  document.body.classList.add('darkmode');
+    document.body.classList.add('darkmode');
 }
 const darkModeSwitch = document.getElementById('darkmode-switch');
 darkModeSwitch.addEventListener('click', () => {
-  document.body.classList.toggle('darkmode');
-  localStorage.setItem('darkMode', document.body.classList.contains('darkmode'));
-  darkModeSwitch.src = document.body.classList.contains('darkmode') ? './Images/profile-pic-blackandwhite.png' : './Images/profile-pic-color.png';
+    document.body.classList.toggle('darkmode');
+    localStorage.setItem('darkMode', document.body.classList.contains('darkmode'));
+    darkModeSwitch.src = document.body.classList.contains('darkmode') ? './Images/profile-pic-blackandwhite.png' : './Images/profile-pic-color.png';
 });
 
 
@@ -15,10 +15,10 @@ darkModeSwitch.addEventListener('click', () => {
 let pressedKeys = '';
 
 window.addEventListener('keyup', (e) => {
-  pressedKeys += e.key;
-  pressedKeys = pressedKeys.slice(-6)
-  if(pressedKeys === "coffee") {
-   alert("You've activated the Java(Script) generator! Prepare for a caffeinated adventure!")
-  }
-  console.log(pressedKeys);
+    pressedKeys += e.key;
+    pressedKeys = pressedKeys.slice(-6)
+    if (pressedKeys === "coffee") {
+        alert("You've activated the Java(Script) generator! Prepare for a caffeinated adventure!")
+    }
+    console.log(pressedKeys);
 })
